035f18e7eeee3bb90bcdc3009f9fe39d75a6949e
taisei.hikawa <cmmkarubi@gmail.com>
2020-09-04 21:11:34 +0900

    Initial Commit

---
+++ BestRamen/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BestRamen
+//
+//  Created by Sakurako Shimbori on 2020/09/04.
+//  Copyright © 2020 Taisei Hikawa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BestRamen/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BestRamen
+//
+//  Created by Sakurako Shimbori on 2020/09/04.
+//  Copyright © 2020 Taisei Hikawa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
