ab23fe2a4430f098eeb96beae828bbdb3206df1a
taisei.hikawa <cmmkarubi@gmail.com>
2020-09-24 11:10:18 +0900

    200924 edit SecondViewController to change icon of pin

--- BestRamen/ViewController/SecondViewController.swift
+++ BestRamen/ViewController/SecondViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import MapKit
 import Firebase
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController ,MKMapViewDelegate{
     
     let db = Firestore.firestore()
     var shopsAry:[Dictionary<String,Any>] = []
@@ -10,6 +10,7 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setShops()
+        mapView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,12 +46,38 @@ class SecondViewController: UIViewController {
             let place = CLLocationCoordinate2D(latitude: latitude ?? 0, longitude: longitude ?? 0)
             let pin: MKPointAnnotation = MKPointAnnotation()
             pin.coordinate = place
-            pin.title = "タイトル"
+            //            pin.
             mapView.addAnnotation(pin)
         }
         
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        //        if (annotation is MKUserLocation) {
+        //            // ユーザの現在地の青丸マークは置き換えない
+        //            return nil
+        //        } else {}
+        // CustomAnnotationの場合に画像を配置
+        // MKPinAnnotationViewを宣言
+        let annoView = MKPinAnnotationView()
+        // MKPinAnnotationViewのannotationにMKAnnotationのAnnotationを追加
+        annoView.annotation = annotation
+        // ピンの画像を変更
+        annoView.image = UIImage(named: "ramenIcon")?.resized(toWidth: self.view.bounds.width/15)
+//        // 吹き出しを使用
+//        annoView.canShowCallout = true
+//        // 吹き出しにinfoボタンを表示
+//        annoView.rightCalloutAccessoryView = UIButton(type: UIButton.ButtonType.detailDisclosure)
+        
+        return annoView
+        //        if annotationView == nil {
+//            annotationView = MKAnnotationView.init(annotation: annotation, reuseIdentifier: identifier)
+//        }
+//        annotationView?.image = UIImage.init(named: "a") // 任意の画像名
+//        annotationView?.annotation = annotation
+//        annotationView!.canShowCallout = true  // タップで吹き出しを表示
+//        return annotationView
+    }
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var searchBar: UISearchBar!
     
