6b314cf043255a00f5b68965bb95c6b8226ea063
taisei-hikawa <cmmkarubi@gmail.com>
2021-03-31 00:43:41 +0900

    add FirebaseManager

---
+++ BestRamen/Model/FirebaseManeger.swift
@@ -0,0 +1,11 @@
+//
+//  FirebaseManeger.swift
+//  BestRamen
+//
+//  Created by 樋川大聖 on 2021/03/31.
+//  Copyright © 2021 Taisei Hikawa. All rights reserved.
+//
+
+class FirebaseManeger {
+    
+}
